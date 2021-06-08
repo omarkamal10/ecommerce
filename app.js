0c74fdb7e9c5e49dce5cf8f557fb7295e20d1190
unknown <omroomessi@gmail.com>
2021-06-08 02:06:24 +0200

    Updated port 2

--- app.js
+++ app.js
@@ -126,9 +126,8 @@ app.use('/user',user)
 
 
 
-//start server
-app.listen(port,()=>{
-  console.log('Server is up on port' + port);
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 
